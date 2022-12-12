f66d2400e27c8f212ebdf00eb394c94d572e97cc
Anton Reva <boss.kvanta@gmail.com>
2022-12-13 01:14:52 +0200

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//Crate && render gallery
+const cardContainer = document.querySelector(".gallery");
+cardContainer.insertAdjacentHTML("afterbegin", createCardsMarkup(galleryItems));
+function createCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `  
+        <div class="gallery__item">
+            <a class="gallery__link" href="#">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`;
+    })
+    .join("");
+}
+
+// //modalWidow
+cardContainer.addEventListener("click", modalWindow);
+function modalWindow(e) {
+  e.preventDefault();
+  const instance = basicLightbox.create(`
+  <div class="modal">
+  <img src="${e.target.dataset.source}" width="800" height="600">
+  </div>`);
+  instance.show();
+
+  const modalPicture = document.querySelector(".modal");
+  modalPicture.addEventListener("click", closeModalWindow);
+  function closeModalWindow(evt) {
+    evt.preventDefault();
+    instance.close();
+    window.removeEventListener("keydown", usingEscape);
+    window.removeEventListener("click", closeModalWindow);
+  }
+  window.addEventListener("keydown", usingEscape);
+  function usingEscape(e) {
+    if (!basicLightbox.visible()) {
+      return;
+    }
+    if (e.key === "Escape" || e.key === "Esc") {
+      e.preventDefault();
+      instance.close();
+      window.removeEventListener("keydown", usingEscape);
+      window.removeEventListener("click", closeModalWindow);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//Crate && render gallery
+const cardContainer = document.querySelector(".gallery");
+
+cardContainer.insertAdjacentHTML("afterbegin", createCardsMarkup(galleryItems));
+
+function createCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `  
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
